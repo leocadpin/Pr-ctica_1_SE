794f3d039fd2d03259470f447dc7de2a3207f9a4
Victoria293 <victoria.nik007@gmail.com>
2022-02-27 22:52:42 +0100

    liada

--- estimation.py
+++ estimation.py
@@ -10,17 +10,26 @@ with np.load('ParamsCamera.npz') as X:
     mtx, dist, _, _ = [X[i] for i in ('mtx','distance','rvecs','tvecs')]
 
 def draw(img, corners, imgpts):
-    corner = tuple(corners[0].ravel())
-    img = cv.line(img, corner, tuple(imgpts[0].ravel()), (255,0,0), 5)
-    img = cv.line(img, corner, tuple(imgpts[1].ravel()), (0,255,0), 5)
-    img = cv.line(img, corner, tuple(imgpts[2].ravel()), (0,0,255), 5)
+    imgpts = np.int32(imgpts).reshape(-1,2)
+
+    # draw ground floor in green
+    img = cv.drawContours(img, [imgpts[:4]],-1,(0,255,0),-3)
+
+    # draw pillars in blue color
+    for i,j in zip(range(4),range(4,8)):
+        img = cv.line(img, tuple(imgpts[i]), tuple(imgpts[j]),(255),3)
+    
+    # draw top layer in red color
+    img = cv.drawContours(img, [imgpts[4:]],-1,(0,0,255),3)
+    
     return img
 
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 objp = np.zeros((hight*wight,3), np.float32)
 objp[:,:2] = np.mgrid[0:wight,0:hight].T.reshape(-1,2)
 
-axis = np.float32([[3,0,0], [0,3,0], [0,0,-3]]).reshape(-1,3)
+axis = np.float32([[0,0,0], [0,3,0], [3,3,0], [3,0,0],
+                   [0,0,-3],[0,3,-3],[3,3,-3],[3,0,-3] ])
 
 for fname in glob.glob('imagenes/*.jpg'):
     img = cv.imread(fname)
