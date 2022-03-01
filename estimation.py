b016f01cc6b8d0ec5d319a7d33692c2994572c03
Victoria293 <victoria.nik007@gmail.com>
2022-03-01 10:41:20 +0100

    Stickman !!!

--- estimation.py
+++ estimation.py
@@ -11,23 +11,23 @@ with np.load('ParamsCamera.npz') as X:
 
 # Definimos la función que dibuja el objeto
 def draw(img, corners, imgpts):
-    imgpts = np.int32(imgpts).reshape(-1, 2)
+    imgpts = np.int32(imgpts).reshape(-1,2)
 
     # draw ground floor in green
-    img = cv.drawContours(img, [imgpts[:4]], -1, (0, 255, 0), -3)
+    img = cv.drawContours(img, [imgpts[:4]], -1, (0,255,0), -3)
 
     # draw pillars in blue color
-    for i,j in zip(range(4),range(4, 8)):
+    for i,j in zip(range(4),range(4,8)):
         img = cv.line(img, tuple(imgpts[i]), tuple(imgpts[j]), (255), 3)
     
     # draw top layer in red color
-    img = cv.drawContours(img, [imgpts[4:]], -1, (0, 0, 255), 3)
+    img = cv.drawContours(img, [imgpts[4:]], -1, (0,0,255), 3)
     
     return img
 
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 objp = np.zeros((hight*wight, 3), np.float32)
-objp[:, :2] = np.mgrid[0:wight, 0:hight].T.reshape(-1, 2)
+objp[:,:2] = np.mgrid[0:wight, 0:hight].T.reshape(-1,2)
 
 axis = np.float32([[0,0,0], [0,3,0], [3,3,0], [3,0,0],
                    [0,0,-3], [0,3,-3], [3,3,-3], [3,0,-3]])
@@ -35,7 +35,7 @@ axis = np.float32([[0,0,0], [0,3,0], [3,3,0], [3,0,0],
 for fname in glob.glob('imagenes/*.jpg'):
     img = cv.imread(fname)
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    ret, corners = cv.findChessboardCorners(gray, (wight, hight), None)
+    ret, corners = cv.findChessboardCorners(gray, (wight,hight), None)
     
     if ret == True:
         corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
