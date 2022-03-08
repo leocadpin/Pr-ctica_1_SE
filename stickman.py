1b891d3a94e71bee62912dd123104082964f0447
leocadpin <leopoldocadavid@gmail.com>
2022-03-08 10:46:14 +0100

    circulitos

--- stickman.py
+++ stickman.py
@@ -6,7 +6,7 @@ wight = 9
 hight = 6
 
 # Load previously saved data
-with np.load('ParamsCamera.npz') as X:
+with np.load('ParamsCamera_patroncirc.npz') as X:
     mtx, dist, _, _ = [X[i] for i in ('mtx','distance','rvecs','tvecs')]
 
 # Definimos la función que dibuja el objeto
@@ -28,12 +28,19 @@ criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 objp = np.zeros((hight*wight,3), np.float32)
 objp[:,:2] = np.mgrid[0:wight,0:hight].T.reshape(-1,2)
 
-axis = np.float32([ [0,0,0],
-                    [0,1,0], [0,3,0],
-                    [0,1,-3], [0,2,-3], [0,3,-3],
-                    [0,1,-6], [0,2,-6], [0,3,-6],
-                    [0,0,-3], [0,4,-3],
-                    [0,2,-7]])
+# axis = np.float32([ [0,0,0],
+#                     [0,1,0], [0,3,0],
+#                     [0,1,-3], [0,2,-3], [0,3,-3],
+#                     [0,1,-6], [0,2,-6], [0,3,-6],
+#                     [0,0,-3], [0,4,-3],
+#                     [0,2,-7]])
+
+axis = np.float32([ [0*72,0*72,0*72],
+                    [0,1*72,0], [0,3*72,0],
+                    [0,1*72,-3*72], [0,2*72,-3*72], [0,3*72,-3*72],
+                    [0,1*72,-6*72], [0,2*72,-6*72], [0,3*72,-6*72],
+                    [0,0,-3*72], [0,4*72,-3*72],
+                    [0,2*72,-7*72]])
 
 cam=cv.VideoCapture(0)
 out = cv.VideoWriter('output.avi',cv.VideoWriter_fourcc('M','J','P','G'), 10, (int(cam.get(3)),int(cam.get(4))))
--- video.py
+++ video.py
@@ -2,11 +2,11 @@ import numpy as np
 import cv2 as cv
 import glob
 
-wight = 9
-hight = 6
+wight = 4
+hight = 11
 
 # Load previously saved data
-with np.load('ParamsCamera.npz') as X:
+with np.load('ParamsCamera_patroncirc.npz') as X:
     mtx, dist, _, _ = [X[i] for i in ('mtx','distance','rvecs','tvecs')]
 
 def draw(img, corners, imgpts):
@@ -28,17 +28,22 @@ criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 objp = np.zeros((hight*wight,3), np.float32)
 objp[:,:2] = np.mgrid[0:wight,0:hight].T.reshape(-1,2)
 
-axis = np.float32([[0,0,0], [0,3,0], [3,3,0], [3,0,0],
-                   [0,0,-3],[0,3,-3],[3,3,-3],[3,0,-3] ])
+# axis = np.float32([[0,0,0], [0,3,0], [3,3,0], [3,0,0],
+#                    [0,0,-3],[0,3,-3],[3,3,-3],[3,0,-3] ])
 
+
+
+axis = np.float32([[0,0,0], [0,3*72,0], [3*72,3*72,0], [3*72,0,0],
+                   [0,0,-3*72],[0,3*72,-3*72],[3*72,3*72,-3*72],[3*72,0,-3*72] ])
 cam=cv.VideoCapture(0)
 out = cv.VideoWriter('output.avi',cv.VideoWriter_fourcc('M','J','P','G'), 10, (int(cam.get(3)),int(cam.get(4))))
+
 while True:
     hasframe,frame=cam.read()
     if hasframe==False:
         break
     gray=cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
-    ret,corners = cv.findChessboardCorners(gray,(9,6),None)
+    ret,corners = cv.findCirclesGrid(gray,(4,11),None)
     if ret == True:
         _,rvec,tvec,_=cv.solvePnPRansac(objp,corners,mtx,dist)
         imgpts,_=cv.projectPoints(axis,rvec,tvec,mtx,dist)
